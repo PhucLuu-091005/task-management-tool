9c972c3fbfb10bbaa763ef77c143d060f143e58b
PhucLuu-091005 <phuc.luu091005@gmail.com>
2026-06-15 23:57:58 +0700

    feat(tasks): add task list/create endpoint with team-board scoping (PR1)

--- backend/apps/tasks/views.py
+++ backend/apps/tasks/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from rest_framework import generics
+
+from apps.tasks.permissions import IsTaskTeamMember, visible_tasks
+from apps.tasks.serializers import TaskSerializer
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsTaskTeamMember]
+
+    def get_queryset(self):
+        return visible_tasks(self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
